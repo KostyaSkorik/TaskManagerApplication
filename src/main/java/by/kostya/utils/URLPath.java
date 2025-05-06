9dffd3760e4f31f40627a33820e1019200317696
Enda Teddy <endatedd@student.21-school.ru>
2025-05-06 10:48:49 +0300

    fix

--- src/main/java/by/kostya/utils/URLPath.java
+++ src/main/java/by/kostya/utils/URLPath.java
@@ -1,5 +1,5 @@
 package by.kostya.utils;
 
 public class URLPath {
-    public static final String REGISTRATION_PATH = "registration";
+    public static final String REGISTRATION_PATH = "/registration";
 }
